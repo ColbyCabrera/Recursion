4a18ce367d4ad9c4797abd1f43d2fc510113bfee
ColbyCabrera <gdthyispro@gmail.com>
2023-07-20 18:45:55 -0400

    Create loop based fib sequence function

--- Assignment1/fib.js
+++ Assignment1/fib.js
@@ -1,3 +1,11 @@
-function fibs() {
-    
+function fibs(n) {
+  let arr = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+
+  return arr;
 }
+
+console.log(fibs(8));
